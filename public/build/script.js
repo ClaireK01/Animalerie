38f27da1ef4e0609f88d3da05c76cafba898b564
Ckalsto01 <ckalsto01@gmail.com>
2022-05-18 16:53:58 +0200

    ' update '

---
+++ public/build/script.js
@@ -0,0 +1,22 @@
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["script"],{
+
+/***/ "./assets/script/main.ts":
+/*!*******************************!*\
+  !*** ./assets/script/main.ts ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+var $ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
+
+__webpack_require__(/*! bootstrap */ "./node_modules/bootstrap/dist/js/bootstrap.esm.js");
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ __webpack_require__.O(0, ["vendors-node_modules_bootstrap_dist_js_bootstrap_esm_js-node_modules_jquery_dist_jquery_js"], () => (__webpack_exec__("./assets/script/main.ts")));
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2NyaXB0LmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBQUEsSUFBSUEsQ0FBQyxHQUFHQyxtQkFBTyxDQUFDLG9EQUFELENBQWY7O0FBQ0FBLG1CQUFPLENBQUMsb0VBQUQsQ0FBUCIsInNvdXJjZXMiOlsid2VicGFjazovLy8uL2Fzc2V0cy9zY3JpcHQvbWFpbi50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJ2YXIgJCA9IHJlcXVpcmUoJ2pxdWVyeScpO1xyXG5yZXF1aXJlKCdib290c3RyYXAnKTtcclxuIl0sIm5hbWVzIjpbIiQiLCJyZXF1aXJlIl0sInNvdXJjZVJvb3QiOiIifQ==
